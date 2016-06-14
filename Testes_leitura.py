53063aa0fd310e3233c3c28ba27a454ed292752f
Gabriel Belini <gabs_belini@hotmail.com>
2016-06-14 12:37:29 -0300

    tcc owl open

--- Testes_leitura.py
+++ Testes_leitura.py
@@ -1,9 +1,8 @@
-
+# -*- coding: utf-8 -*-
 import ontospy
 
 
-g = ontospy.Graph("http://protege.cim3.net/file/pub/ontologies/travel/travel.owl")
-#g = ontospy.Graph("/home/gabsbelini/Documentos/ontologiaTeste.owl")
+g = ontospy.Graph("/home/gabsbelini/Documentos/ontologiaTeste.owl")
 x = g.classes
-print(x)
-print g.datatypeProperties
+for item in x:
+    print(item)
